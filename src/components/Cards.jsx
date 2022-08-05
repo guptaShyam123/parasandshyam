28b942a4692c4db7fcdc24a31de1b418fa1ce773
paras231 <sharmaparas553@gmail.com>
2022-08-05 14:44:03 +0530

    updated card file

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -16,6 +16,21 @@ const Miandiv  = styled.div`
       position: absolute;  
     }
 
+    @media(min-width:1281px) and (max-width:1370px){
+      position: absolute;  
+    }
+
+    @media(min-width:1371px) and (max-width:1440px){
+      position: absolute;  
+    }
+
+    @media(min-width:1441px) and (max-width:1540px){
+      position: absolute;  
+    }
+    @media(min-width:1541px) and (max-width:1640px){
+      position: absolute;  
+    }
+   
 `
 const Image = styled.img`
         height:30%;
@@ -26,6 +41,10 @@ const Image = styled.img`
             width:40%;
         }
         @media(min-width:1025px) and (max-width:1280px){
+          height:30%;
+          width:40%;
+        }
+        @media(min-width:1281px) and (max-width:1370px){
           height:30%;
           width:40%;
         }
@@ -44,6 +63,10 @@ const Bifdiv = styled.div`
     margin-top:10vh;
     margin-left:20vh;
   }
+  @media(min-width:1281px) and (max-width:1370px){
+    margin-top:10vh;
+    margin-left:20vh;
+  }
 `
 
 const Secimg    = styled.div`
@@ -56,6 +79,12 @@ const Secimg    = styled.div`
     left:12%;
   }
   @media(min-width:1025px) and (max-width:1280px){
+    position: absolute;
+    top:11%;
+    left:13%;
+  }
+
+  @media(min-width:1281px) and (max-width:1370px){
     position: absolute;
     top:11%;
     left:13%;
